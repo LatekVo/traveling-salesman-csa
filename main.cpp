818d25c1d8cc73f0c19202402a6df2d430c67f5a
LatekVon <latkaignacy@gmail.com>
2022-08-01 00:40:45 +0200

    - finished most utility functions TODO: * finish jarvis march function * finish deflating algorithm (the core of this project) * create sfml drawing function to display the results

--- main.cpp
+++ main.cpp
@@ -11,12 +11,15 @@
 #include <stdexcept>
 #include <deque>
 #include <map>
+#include <random>
+#include <cmath>
+#include <chrono>
 
 typedef std::pair<float, float> node;
 typedef std::list<node> nodeList;
 
-int setSize = 20;
-int m_size_x = 500, m_size_y = 500;
+int setSize = 16;
+int m_size_x = 512, m_size_y = 512;
 
 // --- --- ---
 // im even attempting this project at the first place, because this algorithm worked every time on paper,
@@ -24,10 +27,33 @@ int m_size_x = 500, m_size_y = 500;
 // perfectly, as this problem is thought to be O(n^2) at best
 // --- --- ---
 
-nodeList getRandomSet(int size) {
-
+// credit: https://iq.opengenus.org/gift-wrap-jarvis-march-algorithm-convex-hull/
+// this is cross product, for determining relative rotation
+float getRotation(node p, node q, node r)
+{
+	// this should be treated binarly: less, more or equal to 0
+	return  (q.second - p.second) * (r.first- q.first) -
+			(q.first- p.first) * (r.second - q.second);
 }
 
+template<typename rngT>
+nodeList getRandomSet(int size, int min, int max, rngT &rng) {
+	nodeList resultSet;
+
+	for (int i = 0; i < size; i++) {
+		resultSet.push_back(
+				// note that this is awfully inefficient, just quick to write
+					{
+						((rng() - rng.min()) / (float) rng.max()) * max + min,
+						((rng() - rng.min()) / (float) rng.max()) * max + min
+					}
+				);
+	}
+
+	return resultSet;
+}
+
+// credit: https://www.tutorialspoint.com/Jarvis-March-Algorithm
 nodeList getConvexHull(nodeList &nodeSet) {
 	node ptr = nodeSet.front();
 
@@ -38,25 +64,73 @@ nodeList getConvexHull(nodeList &nodeSet) {
 
 	}
 
-	nodeList resultSet = {ptr};
+	nodeList resultSet;
+
+	do {
+		 resultSet.push_back(ptr);
 
 
+
+	} while (ptr != resultSet.front());
 
 }
 
-nodeList subtractSet(nodeList &mainSet, nodeList &unwantedSet) {
+nodeList subtractSet(nodeList mainSet, nodeList &unwantedElements) {
+
+	for (auto &item : unwantedElements) {
+		mainSet.remove(item);
+	}
+
+	return mainSet;
+}
+
+float distance(node &a, node &b) {
+	// offset to 0,0
+	float x = b.first - a.first;
+	float y = b.second - a.second;
+
+	// trig vector
+	return std::sqrt(x*x + y*y);
+}
+
+nodeList calculateDeflate(nodeList &hullSet, nodeList &remainingSet) {
+	// 0 -> 1 has index 0
+	std::list<float> lengthList; // list as we need to insert values
+
+	// these are outside to simplify loop below
+	auto it = hullSet.begin();
+	auto last_it = it++;
+
+	while (it != hullSet.end()) {
+
+		lengthList.push_back(distance(*last_it, *it));
+		last_it = it;
+		it++;
+
+	}
+
+	//manually add the last entry
+	lengthList.push_back(distance(hullSet.back(), hullSet.front()));
+
+
 
 }
 
 int main() {
 
-	nodeList initialSet = getRandomSet(setSize);
+	int min = m_size_x < m_size_y ? std::ceil((float)m_size_x * 0.05) : std::ceil((float)m_size_y * 0.05);
+	int max = m_size_x < m_size_y ? std::floor((float)m_size_x * 0.95) : std::floor((float)m_size_y * 0.95);
+
+	std::default_random_engine rng(std::chrono::system_clock::now().time_since_epoch().count());
+
+	nodeList initialSet = getRandomSet(setSize, min, max, rng);
 
 	nodeList convexHullSet = getConvexHull(initialSet);
 	nodeList remainingSet = subtractSet(initialSet, convexHullSet);
 
-	nodeList finalSet;
+	nodeList finalSet = calculateDeflate(convexHullSet, remainingSet);
 
+	/*
 	sf::RenderWindow window(sf::VideoMode(), "gunmo", sf::Style::Fullscreen);
 	window.setFramerateLimit(144);
 
@@ -64,6 +138,7 @@ int main() {
 
 
 	}
+	 */
 
 	return 0;
 }
